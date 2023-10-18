3059918bc56b5633f62cad7f07e7f454e15ac05f
ksenia <ksekat01@mail.ru>
2023-10-18 10:27:49 +0300

    add tasks 4-5

---
+++ MPI/task1.4.cpp
@@ -0,0 +1,57 @@
+﻿#include <iostream>
+#include <mpi.h>
+
+int main(int argc, char** argv) {
+    MPI_Init(&argc, &argv);
+
+    int rank; 
+    int size; 
+
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+    const int n = 10; 
+    int data[n];
+
+    if (rank == 1) {
+        for (int i = 0; i < n; ++i) {
+            data[i] = i;
+        }
+    }
+
+    if (rank != 1) {
+        int recv_count;
+        MPI_Status status;
+
+        MPI_Probe(1, 0, MPI_COMM_WORLD, &status);
+        MPI_Get_count(&status, MPI_INT, &recv_count);
+
+        int* recv_buffer = new int[recv_count];
+
+        MPI_Recv(recv_buffer, recv_count, MPI_INT, 1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+
+       
+        std::cout << "Process " << rank << " received the array: ";
+        for (int i = 0; i < recv_count; ++i) {
+            std::cout << recv_buffer[i] << " ";
+        }
+        std::cout << std::endl;
+
+        
+        delete[] recv_buffer;
+    }
+
+    if (rank == 1) {
+        
+        for (int dest = 0; dest < size; ++dest) {
+            if (dest != 1) {
+                MPI_Send(data, n, MPI_INT, dest, 0, MPI_COMM_WORLD);
+            }
+        }
+    }
+
+    MPI_Finalize();
+
+    return 0;
+
+}
---
+++ MPI/task1.5.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <mpi.h>
+
+int main(int argc, char** argv) {
+    MPI_Init(&argc, &argv);
+
+    int rank;
+    int size;
+
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+    int myNumber = rank * 10; 
+
+    if (rank == 0) {
+        int* receivedNumbers = new int[size];
+        receivedNumbers[0] = myNumber;
+
+        std::cout << "Received numbers in ascending order of ranks:" << std::endl;
+        for (int sender = 1; sender < size; ++sender) {
+            MPI_Recv(&receivedNumbers[sender], 1, MPI_INT, sender, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+            std::cout << "Rank " << sender << ": " << receivedNumbers[sender] << std::endl;
+        }
+
+        delete[] receivedNumbers;
+    }
+    else {
+        MPI_Send(&myNumber, 1, MPI_INT, 0, 0, MPI_COMM_WORLD);
+    }
+
+    MPI_Finalize();
+
+    return 0;
+}
